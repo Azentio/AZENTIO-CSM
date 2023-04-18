a2665481a2b6092f1c2dd2367ab34debee94c56b
ININDC00073 <ININDC00073@10.200.10.43>
2023-04-18 09:53:11 +0530

    Comit2

--- CSM_KamalapriyaS/src/main/java/pageobjects/CSM/AccountsObject.java
+++ CSM_KamalapriyaS/src/main/java/pageobjects/CSM/AccountsObject.java
@@ -9,6 +9,7 @@ public class AccountsObject {
 WebDriver driver;
 
 public AccountsObject(WebDriver driver) {
+
 	this.driver=driver;
 	PageFactory.initElements(driver, this);
 	
@@ -65,6 +66,11 @@ private WebElement generalAccounts_Opening;
 public WebElement generalAccountsOpening() {
 	return generalAccounts_Opening;
 }
+@FindBy(xpath="//span[@id='spanLookup_glType_A002MT']")
+private WebElement generalAccounts_GLtypeSearchButton;
+public WebElement generalAccountsGLTypeSearchButton() {
+	return generalAccounts_GLtypeSearchButton;
+}
 @FindBy(xpath = "//td[text()='Closed Dormant Accounts']//ancestor::li//span[text()='Maintenance']")
 private WebElement closedDormantAccounts_Maintenance;
 
@@ -89,6 +95,35 @@ private WebElement generalAccounts_CurrencyCode;
 public WebElement generalAccountsCurrencyCode() {
 	return generalAccounts_CurrencyCode;
 }
+@FindBy(xpath="//input[@id='lookuptxt_currencyCode_A001MT']")
+private WebElement fixedMaturityAccounts_CurrencyCode;
+
+public WebElement fixedMaturityAccountsCurrencyCode() {
+	return fixedMaturityAccounts_CurrencyCode;
+}
+@FindBy(xpath="//input[@id='lookuptxt_glType_A001MT']")
+private WebElement fixedMaturityAccounts_GLType;
+
+public WebElement fixedMaturityAccountsGLType() {
+	return fixedMaturityAccounts_GLType;
+}
+@FindBy(xpath="//span[@id='spanLookup_glType_A001MT']")
+private WebElement fixedMaturityAccounts_GLType_SearchButton;
+
+public WebElement fixedMaturityAccountsGLTypeSearchButton() {
+	return fixedMaturityAccounts_GLType_SearchButton;
+}
+@FindBy(xpath="//span[@id='spanLookup_currencyCode_A001MT']")
+private WebElement fixedMaturityAccounts_CurrencyCode_SearchButton;
+
+public WebElement fixedMaturityAccountsCurrencyCodeSearchButton() {
+	return fixedMaturityAccounts_CurrencyCode_SearchButton;
+}
+@FindBy(xpath="//span[@id='spanLookup_currencyCode_A002MT']")
+private WebElement generalAccounts_CurrencyCode_SearchButton;
+public WebElement genaralAccountsCurrencyCodeSearchButton() {
+	return generalAccounts_CurrencyCode_SearchButton;
+}
 
 @FindBy(xpath = "//input[@id='lookuptxt_glType_A002MT']")
 private WebElement generalAccounts_GLType;
@@ -109,5 +144,15 @@ private WebElement ClosedDormantAccount_CIFNo;
 
 public WebElement ClosedDormantAccountCIFNo() {
 	return ClosedDormantAccount_CIFNo;
+}
+@FindBy(xpath="//input[@id='accountQueryGrid_Id_A003_gs_GL_CODE']")
+private WebElement accountQueryGLCode;
+public WebElement accountQuery_GLCode() {
+	return accountQueryGLCode;
+}
+@FindBy(xpath="//input[@id='accountQueryGrid_Id_A003_gs_CIF_SUB_NO']")
+private WebElement accountQueryCIFNo;
+public WebElement accountQuery_CIFNo() {
+	return accountQueryCIFNo;
 }
 }
